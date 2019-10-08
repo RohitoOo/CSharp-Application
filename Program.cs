b1c712b4c129bc6747345d7c3a81c7df8f6c166f
Rohit Bhambhani <rohito@Rohitos-MacBook-Pro-Work.local>
2019-10-08 16:01:07 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// Namespace
+namespace RohitoCSharp
+{
+    // Main Class 
+    public class EmptyClass
+    {
+        // Entry Point Method (function)
+
+                // Void = No return value
+                // Static = Referring to the function itself 
+                
+        public EmptyClass()
+        {
+
+            // Start HERE //
+
+            Console.WriteLine("Rohito!!!!!!");
+        }
+    }
+}
